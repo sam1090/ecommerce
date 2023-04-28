f01b0acb70f7046415af451561d5cdb14d28239f
Saumya <usam9010@gmail.com>
2023-04-28 11:38:52 +0530

    product category added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 4000;
 const authRouter = require('./routes/authRoute');
 const productRouter = require('./routes/productRoute');
 const blogRouter = require('./routes/blogRoute')
+const categoryRouter = require('./routes/prodCategoryRoute');
 const bodyParser = require('body-parser');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser');
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use('/api/users',authRouter); 
 app.use('/api/product',productRouter); 
 app.use('/api/blog',blogRouter); 
+app.use('/api/category',categoryRouter); 
 
 app.use(notFound);
 app.use(errorHandler);  
