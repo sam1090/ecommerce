d49e806912df12b59557ee4dca719e0eff8ddd79
Saumya <usam9010@gmail.com>
2023-04-28 12:49:25 +0530

    blog Category added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const authRouter = require('./routes/authRoute');
 const productRouter = require('./routes/productRoute');
 const blogRouter = require('./routes/blogRoute')
 const categoryRouter = require('./routes/prodCategoryRoute');
+const blogCategoryRouter = require('./routes/blogCategoryRoute');
 const bodyParser = require('body-parser');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser');
@@ -24,6 +25,7 @@ app.use('/api/users',authRouter);
 app.use('/api/product',productRouter); 
 app.use('/api/blog',blogRouter); 
 app.use('/api/category',categoryRouter); 
+app.use('/api/blogcategory',blogCategoryRouter); 
 
 app.use(notFound);
 app.use(errorHandler);  
