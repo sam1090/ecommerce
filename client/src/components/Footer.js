85577806c3e2f45f3c1d11f2270a6e16a2b12939
Saumya <usam9010@gmail.com>
2023-06-19 19:56:10 +0530

    working on footer page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,105 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-30 align-items-center">
+                <img src="images/newsletter.png" alt="newsletter" />
+                <h2 className="mb-0 text-white">Sign Up for Newsletter</h2>
+              </div>
+            </div>
+            <div className="col-7">
+              <div class="input-group">
+                <input
+                  type="text"
+                  className="form-control py-1 "
+                  placeholder="Your email address"
+                  aria-label="Your email address"
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-2" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="mb-4 text-white">Contact Us</h4>
+              <div>
+                <address className="text-white fs-6">
+                  Hno: 344 Near White Villa,
+                  <br /> Mumbai <br />
+                  PinCode:116748
+                </address>
+                <a
+                  href="tel : +917684307576"
+                  className="mt-4 text-white mb-2 d-block"
+                >
+                  +917684307576
+                </a>
+                <a
+                  href="mailto:usam12@gmail.com"
+                  className="mt-4 text-white mb-2 d-block"
+                >
+                  usam12@gmail.com
+                </a>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="mb-4 text-white">Information</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white mb-1 py-2">Privacy Policy</Link>
+                <Link className="text-white mb-1 py-2">Refund Policy</Link>
+                <Link className="text-white mb-1 py-2">Shipping Policy</Link>
+                <Link className="text-white mb-1 py-2">
+                  Terms and Conditions
+                </Link>
+                <Link className="text-white mb-1 py-2">Blogs</Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="mb-4 text-white">Account</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white mb-1 py-2">About Us</Link>
+                <Link className="text-white mb-1 py-2">FAQ</Link>
+                <Link className="text-white mb-1 py-2">Contact </Link>
+              </div>
+            </div>
+            <div className="col-2 ">
+              <h4 className="mb-4 text-white">Ouick Links</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white mb-1 py-2">Laptops</Link>
+                <Link className="text-white mb-1 py-2">Headphones</Link>
+                <Link className="text-white mb-1 py-2">Tablets</Link>
+                <Link className="text-white mb-1 py-2">Watch</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy; {new Date().getFullYear()}; Created By Saumya
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
