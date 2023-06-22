9072d85d954245f8ac7aa1957fb17c834ac933f8
Saumya <usam9010@gmail.com>
2023-06-22 23:31:10 +0530

    home page section 2 completed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -94,39 +94,39 @@ const Home = () => {
           <div className="row">
             <div className="col-12">
               <div className="services d-flex align-items-center justify-content-between">
-                <div className="d-flex align-items-center gap-10">
+                <div className="d-flex align-items-center gap-15">
                   <img src="images/service.png" alt="services" />
                   <div>
                     <h6>Free Shipping</h6>
-                    <p className="text-white">From all orders over $5</p>
+                    <p className="mb-0">From all orders over $5</p>
                   </div>
                 </div>
-                <div className="d-flex align-items-center gap-10">
+                <div className="d-flex align-items-center gap-15">
                   <img src="images/service-02.png" alt="services" />
                   <div>
                     <h6>Daily Surprise Offers</h6>
-                    <p>Save upto 25%</p>
+                    <p className="mb-0">Save upto 25%</p>
                   </div>
                 </div>
-                <div className="d-flex align-items-center gap-10">
+                <div className="d-flex align-items-center gap-15">
                   <img src="images/service-03.png" alt="services" />
                   <div>
                     <h6>Support 24/7</h6>
-                    <p>Shop with an expert</p>
+                    <p className="mb-0">Shop with an expert</p>
                   </div>
                 </div>
-                <div className="d-flex align-items-center gap-10">
+                <div className="d-flex align-items-center gap-15">
                   <img src="images/service-04.png" alt="services" />
                   <div>
                     <h6>Affordable Prices</h6>
-                    <p>Get Factory Default Price</p>
+                    <p className="mb-0">Get Factory Default Price</p>
                   </div>
                 </div>
                 <div className="d-flex align-items-center gap-10">
                   <img src="images/service-05.png" alt="services" />
                   <div>
                     <h6>Secure Payment</h6>
-                    <p>100% Protected Payment</p>
+                    <p className="mb-0">100% Protected Payment</p>
                   </div>
                 </div>
               </div>
