ce37b258f90f3622ef61616ff425ba3cdbd52747
Saumya <usam9010@gmail.com>
2023-06-26 18:09:37 +0530

    blog card component completed

---
+++ client/src/components/BlogCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function BlogCard() {
+  return (
+    <div className="col-3">
+      <div className="blog-card">
+        <div className="card-image">
+          <img src="images/blog-1.jpg" alt="blog" className="img-fluid" />
+        </div>
+        <div className="blog-content">
+          <p className="date">09 mar, 2023</p>
+          <h5 className="title">A Beautiful Sunday Morning renaissance</h5>
+          <p className="desc">
+            Nulla ea incididunt officia anim enim et.Minim anim pariatur quis
+            nulla in pariatur mollit cupidatat officia cupidatat esse qui ea
+            exercitation.
+          </p>
+          <Link to="/" className="button">
+            Read More
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BlogCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Marquee from "react-fast-marquee";
-
+import BlogCard from "../components/BlogCard";
 
 const Home = () => {
   return (
@@ -210,33 +210,45 @@ const Home = () => {
             <div className="col-12">
               <div className="marquee-inner-wrapper card-wrapper">
                 <Marquee className="d-flex ">
-                  <div className = "mx-4 w-25">
+                  <div className="mx-4 w-25">
                     <img src="images/brand-01.png" alt="brand" />
                   </div>
-                  <div className = "mx-4 w-25">
+                  <div className="mx-4 w-25">
                     <img src="images/brand-02.png" alt="brand" />
                   </div>
-                  <div className = "mx-4 w-25">
+                  <div className="mx-4 w-25">
                     <img src="images/brand-03.png" alt="brand" />
                   </div>
-                  <div className = "mx-4 w-25">
+                  <div className="mx-4 w-25">
                     <img src="images/brand-04.png" alt="brand" />
                   </div>
-                  <div className = "mx-4 w-25">
+                  <div className="mx-4 w-25">
                     <img src="images/brand-05.png" alt="brand" />
                   </div>
-                  <div className = "mx-4 w-25">
+                  <div className="mx-4 w-25">
                     <img src="images/brand-06.png" alt="brand" />
                   </div>
-                  <div className = "mx-4 w-25">
+                  <div className="mx-4 w-25">
                     <img src="images/brand-07.png" alt="brand" />
                   </div>
-                  <div className = "mx-4 w-25">
+                  <div className="mx-4 w-25">
                     <img src="images/brand-08.png" alt="brand" />
                   </div>
                 </Marquee>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="blog-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="section-heading">Our Latest Blogs</div>
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
           </div>
         </div>
       </section>
