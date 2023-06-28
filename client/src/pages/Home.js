65143da2c3b0dc19c95972c5191d99a909adde57
Saumya <usam9010@gmail.com>
2023-06-28 23:31:35 +0530

    some changes in layout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -218,6 +218,24 @@ const Home = () => {
         </div>
       </section>
 
+      <section className="famous-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="famous-card position-relative">
+                <img src="images/bluetooth.png" alt="famous" className="img-fluid bg-dark "/>
+                <div></div>
+                <div className="famous-content position-absolute">
+                <h5>Big Screen</h5>
+                <h6>JBL BLUETOOTH </h6>
+                <p>From $399or $16.62/mo. for 24 mo.*</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <section className="special-wrapper py-5 home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
@@ -237,16 +255,16 @@ const Home = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
-            <div className="section-heading">Our Popular Products</div>
+              <div className="section-heading">Our Popular Products</div>
             </div>
           </div>
-        
-        <div className="row">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        </div>
+
+          <div className="row">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
         </div>
       </section>
 
