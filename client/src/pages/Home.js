21b790f6fbdf28211057700bcd88d8d56f6ebdf2
Saumya <usam9010@gmail.com>
2023-07-01 12:55:18 +0530

    changes in special wrapper

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -301,6 +301,7 @@ const Home = () => {
             <SpecialProduct />
             <SpecialProduct />
             <SpecialProduct />
+            <SpecialProduct />
           </div>
         </div>
       </section>
