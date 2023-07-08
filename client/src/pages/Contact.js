ab9d97f40f27b08903c10c39e66f5ddff6254d94
Saumya <usam9010@gmail.com>
2023-07-08 23:40:46 +0530

    Contact Page Completed

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,9 +1,103 @@
- import React from 'react'
- 
- const Contact = () => {
-   return (
-     <div>Contact</div>
-   )
- }
- 
- export default Contact
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiPhoneCall } from "react-icons/bi";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+
+const Contact = () => {
+  return (
+    <>
+      <Meta title={"Contact Us"} />
+      <BreadCrumb title="Contact Us" />
+      <div className="contact-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d3502.5291622832597!2d77.3586315!3d28.6138986!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1688833743064!5m2!1sen!2sin"
+                width="600"
+                height="450"
+                className="border-0 w-100 "
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+            <div className="col-12 mt-5">
+              <div className="contact-inner-wrapper d-flex justify-content-between ">
+                <div>
+                  <h3 className="contact-title mb-4">Contact</h3>
+                  <form action="" className="d-flex flex-column gap-15">
+                    <div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="tel"
+                        className="form-control"
+                        placeholder="Mobile Number"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control "
+                        cols="30"
+                        rows="4"
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div>
+                      <button className="button border-0">Submit</button>
+                    </div>
+                  </form>
+                </div>
+                <div>
+                  <h3 className="contact-title mb-4">Get in touch with us</h3>
+                  <div>
+                    <ul className="ps-0">
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineHome className="fs-5" />
+                        <address className="mb-0">
+                          Hno:277 , Near Villa Vista, Noida, Uttar Pradesh
+                        </address>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BiPhoneCall className="fs-5" />
+                        <a href="tel:+91 9678492466">+91 9678492466</a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineMail className="fs-5" />
+                        <a href="mailto:usam9012@gmail.com">usam9012@gmail.com</a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BsInfoCircle className="fs-5" />
+                        <p className="mb-0">Monday - Friday 10 AM - 8PM</p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
