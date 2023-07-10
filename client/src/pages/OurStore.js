193647c583e2fee54584fbd0e21342ffead6ecf7
Saumya <usam9010@gmail.com>
2023-07-10 22:45:26 +0530

    Compare Product Page completed

--- client/src/pages/OurStore.js
+++ client/src/pages/OurStore.js
@@ -3,6 +3,7 @@ import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import ReactStars from "react-rating-stars-component";
 import ProductCard from "../components/ProductCard";
+import Color from "../components/Color";
 
 const OurStore = () => {
   const [grid, setGrid] = useState(4);
@@ -80,23 +81,7 @@ const OurStore = () => {
                   <h5 className="sub-title">Color</h5>
                   <div>
                     <div>
-                      <ul className="colors ps-0">
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                      </ul>
+                      <Color /> 
                     </div>
                   </div>
                   <h5 className="sub-title">Size</h5>
