26f17c015e54605cb1dc6f24fe870c00d9e3e2ad
Saumya <usam9010@gmail.com>
2023-07-11 19:52:10 +0530

    wishlist page completed

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Contact from "./pages/Contact";
 import OurStore from "./pages/OurStore";
 import Blog from "./pages/Blog";
 import CompareProd from "./pages/CompareProd";
+import Wishlist from "./pages/Wishlist";
 function App() {
   return (
     <>
@@ -20,6 +21,7 @@ function App() {
             <Route path="store" element={<OurStore />} />
             <Route path="blogs" element={<Blog />} />
             <Route path="compare-product" element={<CompareProd />} />
+            <Route path="wishlist" element={<Wishlist />} />
           </Route>
         </Routes>
       </BrowserRouter>
