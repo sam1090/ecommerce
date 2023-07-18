5b299a10baa65087161c5b77e5517c12f53f0d5d
Saumya <usam9010@gmail.com>
2023-07-18 19:00:11 +0530

    Single Production description and Review completed

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -9,9 +9,9 @@ const ProductCard = (props) => {
   return (
     <>
       <div
-        className={`${location.pathname == "/:id" ? `gr-${grid}` : "col-3"}`}
+        className={`${location.pathname == "/product" ? `gr-${grid}` : "col-3"}`}
       >
-        <Link to='/:id' className="product-card position-relative">
+        <Link to=':id' className="product-card position-relative">
           <div className="wishlist-icon position-absolute">
             <Link>
               <img src="images/wish.svg" alt="Wishlist" />
@@ -62,8 +62,8 @@ const ProductCard = (props) => {
           </div>
         </Link>
       </div>
-      <div className={`${location.pathname == "/store" ? `gr-${grid}` : "col-3" }`}>
-      <Link className="product-card position-relative">
+      <div className={`${location.pathname == "/product" ? `gr-${grid}` : "col-3" }`}>
+      <Link to=':id'className="product-card position-relative">
         <div className="wishlist-icon position-absolute">
           <Link>
             <img src="images/wish.svg" alt="Wishlist" />
--- client/src/pages/SingleProduct.js
+++ client/src/pages/SingleProduct.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import ProductCard from "../components/ProductCard";
+import ReactStars from "react-rating-stars-component";
 
 const SingleProduct = () => {
+  const [orderedProduct, setorderedProduct] = useState(true);
   return (
     <>
       <Meta title={"Product Name"} />
@@ -11,26 +13,122 @@ const SingleProduct = () => {
       <div className="main-product-wrapper py-5 home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-6"></div> 
+            <div className="col-6"></div>
             <div className="col-6"></div>
           </div>
-          <section className="popular-wrapper  py-5 home-wrapper-2">
-        <div className="container-xxl">
-          <div className="row">
-            <div className="col-12">
-              <div className="section-heading">Our Popular Products</div>
+
+          <section className="description-wrapper py-5 home-wrapper-2">
+            <div className="container-xxl">
+              <div className="row">
+                <div className="col-12">
+                    <h4>Description</h4>
+                  <div className="bg-white p-3">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Sit fugiat nisi obcaecati delectus consequuntur. Unde
+                      inventore, nesciunt, voluptates veritatis modi ab
+                      repellendus velit saepe suscipit ad eius incidunt adipisci
+                      aspernatur.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <div className="reviews-wrapper home-wrapper-2">
+            <div className="container-xxl">
+              <div className="row">
+                <div className="col-12">
+                  <h3>Reviews</h3>
+                  <div className="review-inner-wrapper">
+                    <div className="review-head d-flex justify-content-between align-items-end">
+                      <div>
+                        <h4 className="mb-2">Customer Reviews</h4>
+                        <div className="d-flex gap-10 align-items-center ">
+                          <ReactStars
+                            count={5}
+                            size={24}
+                            value={3}
+                            edit={false}
+                            activeColor="#ffd700"
+                          />
+                          <p className="mb-0"> Based on 100 Reviews</p>
+                        </div>
+                      </div>
+                      {orderedProduct && (
+                        <div>
+                          <a
+                            className="text-dark text text-decoration-underline"
+                            href=""
+                          >
+                            Write a Review
+                          </a>
+                        </div>
+                      )}
+                    </div>
+                    <div className="review-form py-4">
+                      <h4>Write a Review</h4>
+                      <form action="" className="d-flex flex-column gap-15">
+                        <div>
+                          <ReactStars
+                            count={5}
+                            size={24}
+                            value={3}
+                            edit={true}
+                            activeColor="#ffd700"
+                          />
+                        </div>
+                        <div>
+                          <textarea
+                            name=""
+                            id=""
+                            className="w-100 form-control "
+                            cols="30"
+                            rows="4"
+                            placeholder="Comments"
+                          ></textarea>
+                        </div>
+                        <div className="d-flex justify-content-end">
+                          <button className="button border-0">Submit</button>
+                        </div>
+                      </form>
+                    </div>
+                    <div className="reviews">
+                      <div className="review">
+
+                     <div className="d-flex align-items-center gap-10">
+                      <h6 className="mb-0">Saumya</h6>
+                      <ReactStars
+                            count={5}
+                            size={24}
+                            value={3}
+                            edit={false}
+                            activeColor="#ffd700"
+                          />
+                     </div>
+                          <p className="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore aliquam excepturi temporibus ratione. Nihil officiis eius architecto quasi repellendus dolor vel quaerat sunt, ex vitae totam corrupti tempora voluptatem eveniet?</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
 
-          <div className="row">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-          </div>
-        </div>
-      </section>
+          <section className="popular-wrapper  py-5 home-wrapper-2">
+            <div className="container-xxl">
+              <div className="row">
+                <div className="col-12">
+                  <div className="section-heading">Our Popular Products</div>
+                </div>
+              </div>
 
+              <div className="row">
+                <ProductCard />
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </>
