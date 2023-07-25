a13aa26bc14f16e89ecbbf7e65bc04d8c067345f
Saumya <usam9010@gmail.com>
2023-07-25 22:58:04 +0530

    Custom Input Created

---
+++ admin-app/src/Components/CustomInput.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Input } from "antd";
+
+const CustomInput = () => {
+  return <div>CustomInput</div>;
+};
+
+export default CustomInput;
--- admin-app/src/Pages/Login.js
+++ admin-app/src/Pages/Login.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='py-5' style={{background :"$ffd333"}}>
+      <div className='my-5 w-25 bg-white rounded-3 mx-auto p-3'>
+        
+      </div>
+      
+    </div>
   )
 }
 
