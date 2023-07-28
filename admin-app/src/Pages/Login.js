45d9f3081b32c45d23d6a0bdd77559d3eb08e9d2
Saumya <usam9010@gmail.com>
2023-07-29 00:00:28 +0530

    changes in Login Page

--- admin-app/src/Pages/Login.js
+++ admin-app/src/Pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CustomInput from '../Components/CustomInput'
+import Link from 'antd/es/typography/Link'
 
 const Login = () => {
   return (
@@ -12,9 +13,9 @@ const Login = () => {
         <h3 className='text-center'>Login</h3>
         <p className='text-center'>Login to your account to continue</p>
        <form action="">
-       <CustomInput type="text" placeholder="Email Address" id="email" />
-        <CustomInput type="password" placeholder="Password" id="pass" />
-        <button className='border-0 px-3 py-2 text-white fw-bold w-100' style={{background : "#ffd333"}} type='submit'>Login</button>
+       <CustomInput type="text" label="Email Address" id="email" />
+        <CustomInput type="password" label="Password" id="pass" />
+        <Link to='/admin' className='text-center text-decoration-none border-0 px-3 py-2 text-white fw-bold w-100' style={{background : "#ffd333"}} type='submit'>Login</Link>
        </form>
       </div>
       
