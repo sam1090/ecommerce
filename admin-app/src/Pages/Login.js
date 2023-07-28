16131ad8b3cf0337b14bd7094892bb9319ae43a7
Saumya <usam9010@gmail.com>
2023-07-28 23:28:02 +0530

    Login Page Created

--- admin-app/src/Pages/Login.js
+++ admin-app/src/Pages/Login.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import CustomInput from '../Components/CustomInput'
 
 const Login = () => {
   return (
-    <div className='py-5' style={{background :"$ffd333"}}>
-      <div className='my-5 w-25 bg-white rounded-3 mx-auto p-3'>
-        
+    <div className='py-5' style={{background :"#ffd333" , minHeight: "100vh"}}>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className='my-5 w-25 bg-white rounded-3 mx-auto p-4'>
+        <h3 className='text-center'>Login</h3>
+        <p className='text-center'>Login to your account to continue</p>
+       <form action="">
+       <CustomInput type="text" placeholder="Email Address" id="email" />
+        <CustomInput type="password" placeholder="Password" id="pass" />
+        <button className='border-0 px-3 py-2 text-white fw-bold w-100' style={{background : "#ffd333"}} type='submit'>Login</button>
+       </form>
       </div>
       
     </div>
