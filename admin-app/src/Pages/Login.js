6b7c4eb22a6c61b28c55f1ff271d8fa78e1e109f
Saumya <usam9010@gmail.com>
2023-08-23 23:52:58 +0530

    Login Page integrated

--- admin-app/src/Pages/Login.js
+++ admin-app/src/Pages/Login.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CustomInput from "../Components/CustomInput";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 import * as yup from "yup";
 import Password from "antd/es/input/Password";
@@ -9,6 +9,7 @@ import { login } from "../features/auth/authSlice";
 
 const Login = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   let schema = yup.object().shape({
     email: yup
       .string()
@@ -29,6 +30,16 @@ const Login = () => {
       alert(JSON.stringify(values, null, 2));
     },
   });
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+  useEffect(() => {
+    if (!user == null || isSuccess) {
+      navigate("admin ");
+    } else {
+      navigate("not");
+    }
+  }, [user, isLoading, isError, isSuccess, message]);
   return (
     <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
       <br />
@@ -79,56 +90,6 @@ const Login = () => {
       </div>
     </div>
   );
-
-  // con
-  <div className="py-5" style={{ background: "#ffd333", minHeight: "100vh" }}>
-    <br />
-    <br />
-    <br />
-    <br />
-    <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
-      <h3 className="text-center title">Login</h3>
-      <p className="text-center">Login to your account to continue</p>
-      <form action="" onSubmit={formik.handleSubmit}>
-        <CustomInput
-          type="text"
-          name="email"
-          label="Email Address"
-          id="email"
-          onCh={formik.handleChange("email")}
-          val={formik.values.email}
-        />
-        <div className="error">
-          {formik.touched.email && formik.errors.email ? (
-            <div className="">{formik.errors.email}</div>
-          ) : null}
-        </div>
-        <CustomInput
-          type="password"
-          name="password"
-          label="Password"
-          id="pass"
-          onCh={formik.handleChange("password")}
-          val={formik.values.password}
-        />
-        <div className="error">
-          {formik.touched.password && formik.errors.password ? (
-            <div className="">{formik.errors.password}</div>
-          ) : null}
-        </div>
-        <div className="mb-3 text-end">
-          <Link to="forgot-password">Forgot Password?</Link>
-        </div>
-        <button
-          className="text-center text-decoration-none border-0 px-3 py-2 text-white fw-bold w-100 fs-5"
-          style={{ background: "#ffd333" }}
-          type="submit"
-        >
-          Login
-        </button>
-      </form>
-    </div>
-  </div>;
 };
 
 export default Login;
--- admin-app/src/features/auth/authService.js
+++ admin-app/src/features/auth/authService.js
@@ -5,7 +5,12 @@ import { base_url } from "../../utils/baseUrl";
 const login = async(userData) => {
   const response = await axios.post(`${base_url}user/admin-login`, userData);
 
-  console.log(response.data);
+  if(response.data){
+    localStorage.setItem('user' , JSON.stringify(response.data));
+
+  }
+  return response.data;
+  
 }
 
 const authService = {
