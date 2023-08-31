c074d0982ba9a1cea63182ed1a9f0ca4a26d86ff
Saumya <usam9010@gmail.com>
2023-08-31 23:48:11 +0530

    brand list

--- admin-app/src/Pages/BrandList.js
+++ admin-app/src/Pages/BrandList.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Table } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { getBrands } from "../features/brand/brandSlice";
+import Link from "antd/es/typography/Link";
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
+
 
 const columns = [
   {
@@ -23,8 +29,7 @@ const columns = [
 const data1 = [];
 
 for (let i = 0; i < 46; i++) {
-  const dispatch =  
-  data1.push({
+  const dispatch = data1.push({
     key: i,
     name: `Edward King ${i}`,
     product: 32,
@@ -33,6 +38,31 @@ for (let i = 0; i < 46; i++) {
 }
 
 const BrandList = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBrands());
+  }, []);
+  const brandState = useSelector((state) => state.brand.brands);
+  const data1 = [];
+
+for (let i = 0; i < brandState.size; i++) {
+   data1.push({
+    key: i,
+    name: '',
+    product: 32,
+    status: `London, Park Lane no. ${i}`,
+    action: (
+      <>
+        <Link to='/' className="fs-5">
+          <BiEdit />
+        </Link>
+        <Link to= '/' className="ms-3 fs-5 text-danger">
+          <AiFillDelete   />
+        </Link>
+      </>
+    ),
+  });
+}
   return (
     <div>
       <h3 className="mb-4 title">Brands</h3>
