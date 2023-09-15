5ce06b8d7aa9a089ca31ae834b0dba354cc014f0
Saumya <usam9010@gmail.com>
2023-09-15 23:57:01 +0530

    add product

--- admin-app/src/Pages/AddProduct.js
+++ admin-app/src/Pages/AddProduct.js
@@ -4,57 +4,64 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useFormik } from "formik";
 import * as yup from "yup";
-import { useDispatch } from "react-redux";
-import { login } from "../features/auth/authSlice";
-
 
 let schema = yup.object().shape({
-  title: yup
-    .string()
-    .required("Title is required"),
-    description: yup
-    .string()
-    .required("Description is required")
+  title: yup.string().required("Title is required"),
+  description: yup.string().required("Description is required"),
 });
 
-  const AddProduct = () => {
-    const dispatch = useDispatch();
+const AddProduct = () => {
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      description: "",
+    },
+    validationSchema: schema,
 
-    const formik = useFormik({
-      initialValues: {
-        title: "",
-        description: "",
-      },
-      validationSchema: schema,
-  
-      onSubmit: (values) => {
-        dispatch(login(values));
-        alert(JSON.stringify(values, null, 2));
-      },
-    });
-    const [desc, setDesc] = useState();
-    const handleDesc = (e) => {
-      setDesc(e);
-    };
-  
+    onSubmit: (values) => {
+      alert(JSON.stringify(values));
+    },
+  });
+  const [desc, setDesc] = useState();
+  const handleDesc = (e) => {
+    setDesc(e);
+  };
 
   return (
     <div>
       <h3 className="mb-4 title">Add Product</h3>
       <div>
-        <form onSubmit={formik.handleSubmit}>
-          <CustomInput type="text" label="Enter Product Title" name='title' onchng = {formik.handleChange("title")} onBlr = {formik.handleblur("title")} />
-          <div className="mb-3">
-          <ReactQuill
-            theme="snow"
-            value={desc}
-            onChange={(evt) => {
-              handleDesc(evt);
-            }}
+        <form
+          onSubmit={formik.handleSubmit}
+          className="d-flex gap-3 flex-column"
+        >
+          <CustomInput
+            type="text"
+            label="Enter Product Title"
+            name="title"
+            onchng={formik.handleChange("title")}
+            onBlr={formik.handleBlur("title")}
+            val={formik.values.title}
           />
+          <div className="error">
+            {formik.touched.title && formik.errors.title}
           </div>
-        
+          <div className="mb-3">
+            <ReactQuill
+              theme="snow"
+              name="description"
+              onChange={formik.handleChange("description")}
+              onBlur={formik.handleBlur("description")}
+              value={formik.values.description}
+            />
+          </div>
+            <div className="error">
+              {formik.touched.description && formik.errors.description}
+            </div>
           <CustomInput type="number" label="Enter Product Price" />
+          <div className="error">
+              {formik.touched.price && formik.errors.description}
+            </div>
           <select name="" className="form-control py-3 mb-3 " id="">
             <option value="">Select Category</option>
           </select>
@@ -65,106 +72,6 @@ let schema = yup.object().shape({
             <option value="">Select Brand</option>
           </select>
           <CustomInput type="number" label="Enter Product Price" />
-          
-          <button
-            className="btn btn-success border-0 rounded-3 my-5"
-            type="submit"
-          >
-            Add Product
-          </button>
-        </form>
-      </div>
-      <div>
-        <form onSubmit={formik.handleSubmit}>
-          <CustomInput type="text" label="Enter Product Title" name='title' onchng = {formik.handleChange("title")} onBlr = {formik.handleblur("title")} />
-          <div className="mb-3">
-          <ReactQuill
-            theme="snow"
-            value={desc}
-            onChange={(evt) => {
-              handleDesc(evt);
-            }}
-          />
-          </div>
-        
-          <CustomInput type="number" label="Enter Product Price" />
-          <select name="" className="form-control py-3 mb-3 " id="">
-            <option value="">Select Category</option>
-          </select>
-          <select name="" className="form-control py-3 mb-3 " id="">
-            <option value="">Select Color</option>
-          </select>{" "}
-          <select name="" className="form-control py-3 mb-3 " id="">
-            <option value="">Select Brand</option>
-          </select>
-          <CustomInput type="number" label="Enter Product Price" />
-          
-          <button
-            className="btn btn-success border-0 rounded-3 my-5"
-            type="submit"
-          >
-            Add Product
-          </button>
-        </form>
-      </div>
-      <div>
-        <form onSubmit={formik.handleSubmit}>
-          <CustomInput type="text" label="Enter Product Title" name='title' onchng = {formik.handleChange("title")} onBlr = {formik.handleblur("title")} />
-          <div className="mb-3">
-          <ReactQuill
-            theme="snow"
-            value={desc}
-            onChange={(evt) => {
-              handleDesc(evt);
-            }}
-          />
-          </div>
-        
-          <CustomInput type="number" label="Enter Product Price" />
-          <select name="" className="form-control py-3 mb-3 " id="">
-            <option value="">Select Category</option>
-          </select>
-          <select name="" className="form-control py-3 mb-3 " id="">
-            <option value="">Select Color</option>
-          </select>{" "}
-          <select name="" className="form-control py-3 mb-3 " id="">
-            <option value="">Select Brand</option>
-          </select>
-          <CustomInput type="number" label="Enter Product Price" />
-          
-          <button
-            className="btn btn-success border-0 rounded-3 my-5"
-            type="submit"
-          >
-            Add Product
-          </button>
-        </form>
-      </div>
-      <div>
-        <form onSubmit={formik.handleSubmit}>
-          <CustomInput type="text" label="Enter Product Title" name='title' onchng = {formik.handleChange("title")} onBlr = {formik.handleblur("title")} />
-          <div className="mb-3">
-          <ReactQuill
-            theme="snow"
-            value={desc}
-            onChange={(evt) => {
-              handleDesc(evt);
-            }}
-          />
-          </div>
-        
-          <CustomInput type="number" label="Enter Product Price" />
-          <select name="" className="form-control py-3 mb-3 " id="">
-            <option value="">Select Category</option>
-          </select>
-          <select name="" className="form-control py-3 mb-3 " id="">
-            <option value="">Select Color</option>
-          </select>{" "}
-          <select name="" className="form-control py-3 mb-3 " id="">
-            <option value="">Select Brand</option>
-          </select>
-          <CustomInput type="number" label="Enter Product Price" />
-          
           <button
             className="btn btn-success border-0 rounded-3 my-5"
             type="submit"
