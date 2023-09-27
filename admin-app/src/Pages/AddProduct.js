22023d3c4326b70fc7ec25f948396b89de901f13
Saumya <usam9010@gmail.com>
2023-09-27 19:54:47 +0530

    Add product

--- admin-app/src/Pages/AddProduct.js
+++ admin-app/src/Pages/AddProduct.js
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CustomInput from "../Components/CustomInput";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useFormik } from "formik";
 import * as yup from "yup";
+import { useDispatch, useSelector } from "react-redux";
+import { getBrands } from "../features/brand/brandSlice";
+import useSelection from "antd/es/table/hooks/useSelection";
 
 let schema = yup.object().shape({
   title: yup.string().required("Title is required"),
   description: yup.string().required("Description is required"),
+  price: yup.number().required("Price is Required"),
 });
 
 const AddProduct = () => {
-  const formik = useFormik({
+  const dispatch = useDispatch();
+  
+  useEffect(() =>{
+    dispatch(getBrands());
+
+  },[]);
+  const brandState = useSelector((state) => state.brand.brands.getAllBrand || []);
+  const[brand , setBrand] = useState([]);
+   const formik = useFormik({
     initialValues: {
       title: "",
       description: "",
+      price: "",
     },
     validationSchema: schema,
 
@@ -58,9 +71,12 @@ const AddProduct = () => {
             <div className="error">
               {formik.touched.description && formik.errors.description}
             </div>
-          <CustomInput type="number" label="Enter Product Price" />
+          <CustomInput type="number" label="Enter Product Price" name="price"
+              onChange={formik.handleChange("price")}
+              onBlur={formik.handleBlur("price")}
+              value={formik.values.price} />
           <div className="error">
-              {formik.touched.price && formik.errors.description}
+              {formik.touched.price && formik.errors.price}
             </div>
           <select name="" className="form-control py-3 mb-3 " id="">
             <option value="">Select Category</option>
@@ -70,6 +86,9 @@ const AddProduct = () => {
           </select>{" "}
           <select name="" className="form-control py-3 mb-3 " id="">
             <option value="">Select Brand</option>
+            {brandState.map((i, j) =>{
+              return <option key ={j} value ="{i.title}"> {i.title}</option>
+            })}
           </select>
           <CustomInput type="number" label="Enter Product Price" />
           <button
